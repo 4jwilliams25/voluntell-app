d2aa80fbe27bca97a18df33be7fb6e6eb1d4b6a4
Alexander Williams <4jwilliams25@gmail.com>
2019-12-07 10:26:13 -0700

    User Profile Page Started and Event List started

--- volunteer-app-client/src/components/user/UserEventList.js
+++ volunteer-app-client/src/components/user/UserEventList.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { useSelector } from 'react-redux';
+import UserEvent from './UserEvent';
 
-const UserEventList = () => {
+const UserEventList = props => {
+    // Get the logged in user's events
+    const events = useSelector(state => state.events);
+    const user_events = useSelector(state => state.userEvents);
+    const thisUserEvents = events && user_events && user_events.filter(event => event.user_id === props.user.id).map(event => {
+        for (let i = 0; i < events.length; i++) {
+            if (events[i].id === event.event_id) {
+                return events[i]
+            }
+        }
+    })
+    // Create the list of this user's events
+    const usersEventList = thisUserEvents && thisUserEvents.map(event => <UserEvent eventDetails={event} />)
+
     return (
-        <div>UserEventList</div>
+    <div>{usersEventList}</div>
     )
 }
 
--- volunteer-app-client/src/components/user/UserProfile.js
+++ volunteer-app-client/src/components/user/UserProfile.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { Jumbotron } from 'reactstrap';
+import { useSelector } from 'react-redux';
+import UserEventList from './UserEventList';
 
 const UserProfile = () => {
+    const users = useSelector(state => state.users)
+    const user = users.filter(user => user.id === 1)[0]
+
     return (
-        <div>UserProfile</div>
+        <div>
+            <Jumbotron className="m-3">
+                <h1 className="display-3">{user && user.name}</h1>
+                <p>{user && user.phone}</p>
+                <p>{user && user.email}</p>
+            </Jumbotron>
+            <UserEventList user={user} />
+        </div>
     )
 };
 
